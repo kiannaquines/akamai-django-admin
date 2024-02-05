833d2286df8fbf07cae5fd030fba89c17db91957
kiannaquines <kjgnaquines@gmail.com>
2024-02-05 01:33:27 -0500

    changes to elements

--- static/js/akamai.main.js
+++ static/js/akamai.main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     document.querySelectorAll('.sidebar-item').forEach(item => {
         if(item.classList.contains('active')){
-            alert("Naay active")
+            // alert("Naay active")
         }
     });
 }
@@ -17,7 +17,14 @@ document.querySelectorAll('.sidebar-item').forEach(item => {
     });
 });
 
-document.querySelector('.toggle-navbar').addEventListener('click', function(){
-    document.querySelector('.navbar').classList.add('w-full')
+document.querySelector('.toggle-navbar').addEventListener('click', function(e){
     document.querySelector('.sidebar').classList.add('hidden')
+    document.querySelector('.navbar').classList.add('w-full')
+});
+
+
+document.querySelector('.search').addEventListener('click',function(e){
+    const parent = document.querySelector('.search-area')
+    parent.classList.remove('hidden')
+    parent.classList.add('inline')
 });
